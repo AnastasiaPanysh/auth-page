c1666f951a20638cbd66ec3d1044b33aae4825a7
anastasiya <nastya.panysh@gmail.com>
2022-10-05 00:06:39 +0300

    add css

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector('btn')
+const btn = document.querySelector('.btn')
 
 function checkFirstInput(emailPhonenumber) {
     if (!/^([a-zA-Z0-9\.\_\-]+@[a-z]+\.[a-z]{1,3})|(\+[0-9]{12})$/g.test(emailPhonenumber.value)) throw new Error('login failed');
@@ -10,13 +10,10 @@ function checkPassword(password) {
     if (!password.value) throw new Error('enter password');
 }
 
-
-
 btn.addEventListener('click', () => {
-    const emailPhonenumber = document.querySelector('.input-email')
-    const password = document.querySelector('.input-password')
-
     try {
+        const emailPhonenumber = document.querySelector('.input-email')
+        const password = document.querySelector('.input-password')
         checkFirstInput(emailPhonenumber)
         checkPassword(password)
         alert('login successful')
