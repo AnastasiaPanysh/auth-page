933720f087a455c9743aa2d4367f1525f91a9660
anastasiya <nastya.panysh@gmail.com>
2022-10-05 00:23:34 +0300

    edit app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector('.btn')
+const btn = document.querySelector('.btn');
 
 function checkFirstInput(emailPhonenumber) {
     if (!/^([a-zA-Z0-9\.\_\-]+@[a-z]+\.[a-z]{1,3})|(\+[0-9]{12})$/g.test(emailPhonenumber.value)) throw new Error('login failed');
@@ -11,17 +11,17 @@ function checkPassword(password) {
 }
 
 btn.addEventListener('click', () => {
-    const emailPhonenumber = document.querySelector('.input-email')
-    const password = document.querySelector('.input-password')
+    const emailPhonenumber = document.querySelector('.input-email');
+    const password = document.querySelector('.input-password');
     try {
         checkFirstInput(emailPhonenumber)
         checkPassword(password)
-        alert('login successful')
-        password.value = ''
-        emailPhonenumber.value = ''
+        alert('login successful');
+        password.value = '';
+        emailPhonenumber.value = '';
     } catch (error) {
-        alert(error.message)
-        password.value = ''
-        emailPhonenumber.value = ''
+        alert(error.message);
+        password.value = '';
+        emailPhonenumber.value = '';
     }
 })
